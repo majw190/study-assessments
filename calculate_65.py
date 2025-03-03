bf1a0482bcc1a43e114e39b87ead883765a75bee
thenextprevious <76392789+majw190@users.noreply.github.com>
2025-03-03 22:53:25 +0000

    Update calculate_65.py

--- calculate_65.py
+++ calculate_65.py
@@ -2,9 +2,11 @@
 
 def calculate_sum(n):
     total = 0
+    // calculate sum of products of all pairs
     for i in range(1, n):
       for j in range(i + 1, n):
         total += i * j
     return total
-  result = calculate_sum(5)
-  print(result)
+
+result = calculate_sum(5)
+print(result) 
